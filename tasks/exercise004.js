2dba89dac2c8490645e1e04c92d4a8f06812fae5
Rookiejlm <37531289+Rookiejlm@users.noreply.github.com>
2020-11-25 13:42:16 +0000

    Update exercise004.js

--- tasks/exercise004.js
+++ tasks/exercise004.js
@@ -1,7 +1,13 @@
 // Move the first letter of each word to the end of it, then add "ay" to the end of the word. Leave punctuation marks untouched.
 
 function pigLatin(str){
-    // You code here!
+    str = str.split('');
+    for(let i=0; i<str.length; i++){
+    if(/[a-zA-Z]/.test(str[i])){
+    str[i] = str[i].slice(1) + str[i][0] + 'ay';
+    }
+    }
+    return str.join('');
 }
 
 module.exports = {
